6797249d98f19aef52b264be9f423114aec94461
OrionAlzate <orion_alzate@hotmail.com>
2023-03-17 20:42:24 -0500

    Se  crea index

---
+++ src/AcercaDe/AcercaDe.js
@@ -0,0 +1,21 @@
+export function AcercaDe(){
+    return(
+        <section>
+            <div class ="container my-5">
+                <div class ="row justify-content-center">
+
+                    <div class ="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/comicstore-aff4a.appspot.com/o/manga-death-note.webp?alt=media&token=1b7e1cf9-7a95-49c5-a34c-9b1570532f74" alt="foto" class="img-fluid"/>
+                    </div>
+
+                    <div class ="col-12 col-md-4 text-center align-self-center">
+                        <h2>Makitu Comic Store</h2>
+                        <p>Descripción</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+    )
+}
---
+++ src/Servicios/Servicios.js
@@ -0,0 +1,30 @@
+import './Servicios.css'
+export function Servicios(){
+    return(
+
+        <>
+            <section class="container-fluid my-5">
+                <div class="row text-center justify-content-around">
+                    <div class ="col-12 col-md-2 fondo" >
+                        <h2>Comics</h2>
+                        <i class="bi bi-credit-card-2-front fs-1"></i>
+                    </div>
+                    <div class ="col-12 col-md-2 fondo">
+                        <h2>Manga</h2>
+                        <i class="bi bi-credit-card-2-front fs-1"></i>
+                    </div>
+                    <div class ="col-12 col-md-2 fondo">
+                        <h2>Libros</h2>
+                        <i class="bi bi-credit-card-2-front fs-1"></i>
+                    </div>
+                    <div class ="col-12 col-md-2 fondo">
+                        <h2>Accesorios</h2>
+                        <i class="bi bi-credit-card-2-front fs-1"></i>
+                    </div>
+                </div>
+            </section>
+        
+        
+        </>
+    )
+}
